31486c2cabc2995eba854306036e073fb3071b82
Elisha Okon <eliscodavid@gmail.com>
2023-05-03 00:29:09 +0100

    Update index.jsx

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <Box mt='130px' py='30px' w='full'  >
       <VStack gap='30px' w='full'>
         <Image src='/assets/ARBFURBO.png' height={500} width={500} alt='Atap Logo' />
-        <Text textColor={'brand.200'} fontWeight={200}>Our Predecessor $FARB gave us the idea of rewarding our holders and we added a twist. Holders get rewarded in both $FARB and $ARB tokens</Text>
+        <Text textColor={'brand.200'} maxW={"450px"} fontWeight={200}>Missed out on $FARB? Don't miss this!</Text>
         <Button px='20px' bg='brand.200' fontWeight={400}>JOIN OUR COMMUNITY</Button>
       </VStack>
     </Box>
