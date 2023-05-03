a0816dfc373d15c3ac175d3a721830b7b7c2bbc8
Elisha Okon <eliscodavid@gmail.com>
2023-05-03 12:01:51 +0100

    Update index.jsx

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <Box mt='130px' py='30px' w='full'  >
       <VStack gap='30px' w='full'>
         <Image src='/assets/ARBFURBO.png' height={500} width={500} alt='Atap Logo' />
-        <Text textColor={'brand.200'} maxW={"450px"} fontWeight={200}>If you missed out on $FARB, Don$apos;t miss out on this</Text>
+        <Text textColor={'brand.200'} maxW={"450px"} fontWeight={200}>If you missed out on $FARB, Do not miss out on this</Text>
         <Button px='20px' bg='brand.200' fontWeight={400}>JOIN OUR COMMUNITY</Button>
       </VStack>
     </Box>
