54222110faeaa4b46277fc6e33044c32ac5cc4fe
Elisha Okon <eliscodavid@gmail.com>
2023-05-03 11:36:10 +0000

    Done more changes

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <Box mt='130px' py='30px' w='full'  >
-      <VStack gap='30px' w='full'>
-        <Image src='/assets/ARBFURBO.png' height={500} width={500} alt='Atap Logo' />
-        <Text textColor={'brand.200'} maxW={"450px"} fontWeight={200}>If you missed out on $FARB, Do not miss out on this</Text>
-        <Button px='20px' bg='brand.200' fontWeight={400}>JOIN OUR COMMUNITY</Button>
+    <Box mt='5px' bgColor={'brand.200'} py='10px' w='full'>
+      <VStack  w='full'>
+        <Image src='/assets/ARBFURBO.png' height={500} width={800} alt='Atap Logo' />
+        {/* <Text textColor={'brand.700'} maxW={"420px"} fontSize={"120px"}  fontWeight={800}>BABY FURBO</Text> */}
+        <Button px='20px' bg='brand.700' fontWeight={400}>BUY $BFARB</Button>
       </VStack>
     </Box>
   )
