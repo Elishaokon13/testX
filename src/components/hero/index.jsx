f2f4a84d93f4d43ab2ae9c622b5a0acf0ac536cd
hicode <110273715+Hicode-dev@users.noreply.github.com>
2023-05-12 14:03:56 +0000

    working on the first tab

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,18 +1,28 @@
-import { Box, Button, Flex, Heading, Tag, TagLabel, Text, VStack } from '@chakra-ui/react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+// import { Box, Button, Flex, Heading, Tag, TagLabel, Text, VStack } from '@chakra-ui/react'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import React from 'react'
 
 export default function Hero() {
   return (
-    <Box mt='5px' bgColor={'brand.200'} py='10px' w='full'>
-      <VStack  w='full'>
-        <Image src='/assets/ARBFURBO.png' height={500} width={800} alt='Atap Logo' />
-        {/* <Text textColor={'brand.700'} maxW={"420px"} fontSize={"120px"}  fontWeight={800}>BABY FURBO</Text> */}
-        <Link href='https://app.sushi.com/swap?outputCurrency=0x721ebf5c9fa9b4c93194cbfae4060638374fbd61&chainId=1'>
-        <Button px='20px' w={"200px"} bg='brand.700' fontWeight={400}>BUY $BFARB</Button>
-        </Link>
-      </VStack>
-    </Box>
+    <div className=" mt-5 bg-brand-100 py-10 ">
+<div>
+<div className="bg-[rgb(21,8,30)]">
+    <div className="justify-center flex-col flex h-[30rem] items-center">
+    <h1 className="text-brand-700 py-10  max-w-420 mt-3 text-[2rem] font-[700px]">The project that inspired the modern CryptoArt movement</h1>
+    
+    <p className=" text-center">10,000 unique collectible characters with proof of ownership stored <br /> on the Ethereum blockchain.</p>
+    <a className="pt-[1rem] " href="https://app.sushi.com/swap?outputCurrency=0x721ebf5c9fa9b4c93194cbfae4060638374fbd61&chainId=1">
+      <button className="py-4 px-8 rounded-lg  w-200 bg-blue-700 font-medium">BUY $BFARB</button>
+    </a>
+    </div>
+    </div>
+
+</div>
+
+</div>
+
+
+  
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,9 @@ export default function Home() {
           <Hero />
           <AboutSection />
           <Roadmap />
+          <Community />
+          <Team />
+          <Faqs />
           
         </PageLayout>
       </main>
