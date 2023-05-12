1064873cf60116138ef93ba13b4135275b44c0fe
hicode <110273715+Hicode-dev@users.noreply.github.com>
2023-05-12 20:33:02 +0000

    third part right now

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,7 +2,7 @@ import { Box, Heading, VStack, Text, Flex } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import Image from 'next/image'
 import React from 'react'
-
+import {BsPhone,BsFire} from 'react-icons/bs'
 export default function AboutSection() {
     return (
         // <Box my='50px'>
@@ -96,29 +96,67 @@ export default function AboutSection() {
     </a>
 </div>
 
-<div className="grid grid-cols-2">
+<div className="grid grid-cols-1 md:grid-cols-2 mt-6 px-10 ">
     
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div className="max-w-sm bg-white  border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
     <Image
-    src='/assets/dd.png'
+    src='https://th.bing.com/th/id/OIP.pn-cMd0gmnL_9cUSFXHKXQHaFj?pid=ImgDet&rs=1'
     width={400}
     height={500}
-    alt='Profile'
+
     />
     </a>
     <div className="p-5">
         <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">What is BABY FURBO?</h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Our Predecessor, $FARB gave us the idea of rewarding our holders and we added a twist. Holders get rewarded in both $FARB and $ARB tokens</p>
         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </a>
     </div>
 </div>
+<div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-[2rem] mt-20">
+    
+<div className="max-w-sm p-6 bg-white border border-gray-200 h-fit rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div>
+        <BsPhone size={20} b color='blue' />
+    </div>
+    <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Supply</h5>
+    </a>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">10,000,000,000</p>
+    
+</div>
+<div className="max-w-sm p-6 bg-white border border-gray-200 h-fit rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div>
+        <BsFire size={20} color='red' />
+    </div>
+    <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Burn</h5>
+    </a>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">40%</p>
+    
+</div>
+<div className="max-w-sm p-6 bg-white border border-gray-200 h-fit rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Supply </h5>
+    </a>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">50%</p>
+    
+</div>
+<div className="max-w-sm p-6 bg-white border border-gray-200 h-fit rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Supply</h5>
+    </a>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">10,000,000,000</p>
+    
+</div>
 
+
+</div>
 </div>
     </div>
  </div>
