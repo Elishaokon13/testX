c0cbfff58e81b265e8e5df5d13097df5fccbf6a4
Elisha Okon <eliscodavid@gmail.com>
2023-05-14 21:48:44 +0100

    Update index.jsx

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -10,11 +10,11 @@ export default function Hero() {
 <div>
 <div className="bg-[rgb(21,8,30)] grid md:grid-cols-2 grid-cols-1 pt-14 place-content-between place-items-center  ">
     <div className="ml-6">
-    <h1 className="text-brand-700 l py-10 max-w-420 mt-3 text-6xl font-bold">Your gateway to ARBFURBO & beyond</h1>
+    <h1 className="text-brand-700 l py-10 max-w-420 mt-3 text-6xl font-bold">HO LEE $HEET</h1>
     
-    <p className=" ">The tools and information you need to buy, sell, trade, invest, and spend ARBFURBO</p>
+    <p className=" ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis diam non diam facilisis, sed venenatis magna laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
     <a className="pt-[1rem] " href="https://app.sushi.com/swap?outputCurrency=0x721ebf5c9fa9b4c93194cbfae4060638374fbd61&chainId=1">
-      <button className="py-4 px-8 rounded-lg  w-200 bg-blue-700 font-medium mt-6">BUY $BFARB</button>
+      <button className="py-4 px-8 rounded-lg  w-200 bg-blue-700 font-medium mt-6">BUY $HEET</button>
     </a>
     </div>
     <div className='mt-8 md:mt-0'>
